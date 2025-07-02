49dc0084e7e0986b6d3d5b13471689f6267f0b45
Lívia Lelis <git@lelis.dev>
2025-07-02 10:51:14 -0300

    include html presentation

--- presentation/build.sh
+++ presentation/build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 npx @marp-team/marp-cli@latest slides.md -o slides.pdf --theme-set=style/ --allow-local-files
+npx @marp-team/marp-cli@latest slides.md -o slides.html --theme-set=style/ --allow-local-files
+
